23d8c0f27f2cbd94738ae23d688aeb0e38806d37
Shuji Watanabe for int <176282865+ShujiWatanabe97@users.noreply.github.com>
2026-08-26 12:30:30 +0900

    feat(care): add Smareha hospital shares

--- apps/demo/public/minnade-kaigo/sw.js
+++ apps/demo/public/minnade-kaigo/sw.js
@@ -1,4 +1,4 @@
-const CACHE='minnade-kaigo-v1';
+const CACHE='minnade-kaigo-v2';
 const ASSETS=['/minnade-kaigo/','/minnade-kaigo/styles.css','/minnade-kaigo/personas.js','/minnade-kaigo/app.js','/minnade-kaigo/manifest.json','/minnade-kaigo/icon.svg','/minnade-kaigo/personas/demo1-aoki-kazuko.png','/minnade-kaigo/personas/demo2-yoshida-osamu.png','/minnade-kaigo/personas/demo3-fujimoto-chiyo.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS))));
 self.addEventListener('fetch',event=>event.respondWith(fetch(event.request).catch(()=>caches.match(event.request))));
